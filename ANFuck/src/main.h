8d66b4354e988117f12508f7cc5f4deaa3b8229f
ANF-Studios <theanfstudios@gmail.com>
2021-02-23 17:19:38 -0500

    Formatted code

--- ANFuck/src/main.h
+++ ANFuck/src/main.h
@@ -31,81 +31,81 @@ namespace ANFuck {
 		{
 			switch (*instructionPtr)
 			{
-			case '<':
-			{
-				dataPtr--;
-				break;
-			}
-			case '>':
-			{
-				dataPtr++;
-				if (dataPtr == data.end()) {
-					data.push_back(0);
-					dataPtr = data.end() - 1;
-				}
-				break;
-			}
-			case '+':
-			{
-				(*dataPtr) += 1;
-				break;
-			}
-			case '-':
-			{
-				(*dataPtr) -= 1;
-				break;
-			}
-			case '.':
-			{
-				std::cout << char(*dataPtr);
-				break;
-			}
-			case ',':
-			{
-				char input;
-				std::cin >> input;
-				*dataPtr = input;
-			}
-			case '[':
-			{
-				instructionStack.push(instructionPtr);
-
-				if (*dataPtr == 0)
+				case '<':
 				{
-					auto startInstructionPtr = instructionPtr;
-					while (++instructionPtr != commands.end())
+					dataPtr--;
+					break;
+				}
+				case '>':
+				{
+					dataPtr++;
+					if (dataPtr == data.end()) {
+						data.push_back(0);
+						dataPtr = data.end() - 1;
+					}
+					break;
+				}
+				case '+':
+				{
+					(*dataPtr) += 1;
+					break;
+				}
+				case '-':
+				{
+					(*dataPtr) -= 1;
+					break;
+				}
+				case '.':
+				{
+					std::cout << char(*dataPtr);
+					break;
+				}
+				case ',':
+				{
+					char input;
+					std::cin >> input;
+					*dataPtr = input;
+				}
+				case '[':
+				{
+					instructionStack.push(instructionPtr);
+
+					if (*dataPtr == 0)
 					{
-						if (*instructionPtr == '[')
-							instructionStack.push(instructionPtr);
-						else if (*instructionPtr == ']')
+						auto startInstructionPtr = instructionPtr;
+						while (++instructionPtr != commands.end())
 						{
-							if (instructionStack.empty())
-								throw std::runtime_error("Found a ']' that did not have a matching '['!");
+							if (*instructionPtr == '[')
+								instructionStack.push(instructionPtr);
+							else if (*instructionPtr == ']')
+							{
+								if (instructionStack.empty())
+									throw std::runtime_error("Found a ']' that did not have a matching '['!");
 
-							auto tempInstructionPtr = instructionStack.top();
-							instructionStack.pop();
+								auto tempInstructionPtr = instructionStack.top();
+								instructionStack.pop();
 
-							if (startInstructionPtr == tempInstructionPtr)
-								break;
+								if (startInstructionPtr == tempInstructionPtr)
+									break;
+							}
 						}
 					}
+					break;
 				}
-				break;
-			}
-			case ']':
-			{
-				if (instructionStack.empty())
-					throw std::runtime_error("Found a ']' that did not have a matching '['!");
+				case ']':
+				{
+					if (instructionStack.empty())
+						throw std::runtime_error("Found a ']' that did not have a matching '['!");
 
-				if (*dataPtr != 0)
-					instructionPtr = instructionStack.top();
-				else
-					instructionStack.pop();
+					if (*dataPtr != 0)
+						instructionPtr = instructionStack.top();
+					else
+						instructionStack.pop();
 
-				break;
-			}
-			default:
-				break;
+					break;
+				}
+				default:
+					break;
 			}
 
 			instructionPtr++;
