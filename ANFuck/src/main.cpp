bd2b2dc289b22f97f38a7f2cd563c60253a3968d
ANF-Studios <theanfstudios@gmail.com>
2021-02-23 18:42:36 -0500

    Renamed function

--- ANFuck/src/main.cpp
+++ ANFuck/src/main.cpp
@@ -26,14 +26,14 @@ int main(int argc, char* argv[]) {
 	if (argc != 2 || helpType == (std::string)"help") std::cout << HelpMessage;
 	else if (helpType == (std::string)"version") std::cout << Version;
 	else if (helpType == (std::string)"path/to/brainfuck/code") std::cout << "You have to enter the actual path of your code";
-	else ParsePath((std::string)argv[1]);
+	else GetAndRunCode((std::string)argv[1]);
 
 #ifdef ANFUCK_DEBUG
 	auto _ = _getch();
 #endif
 }
 
-void ParsePath(const std::string& pathToFile) {
+void GetAndRunCode(const std::string& pathToFile) {
 	try
 	{
 		std::string commands = ANFuck::LoadFile(pathToFile);
