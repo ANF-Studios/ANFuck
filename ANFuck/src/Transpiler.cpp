9c5ead3e670bb3a230a10ba17c5786f51892454c
ANF-Studios <theanfstudios@gmail.com>
2021-03-21 02:39:49 -0400

    Created initial transpiler

---
+++ ANFuck/src/Transpiler.cpp
@@ -0,0 +1,10 @@
+#include "Transpiler.h"
+#include <stdexcept>
+
+namespace ANFuck
+{
+	std::string Transpiler::Transpile()
+	{
+		throw new std::runtime_error("Not implemented!");
+	}
+}
---
+++ ANFuck/src/Transpiler.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <xstring>
+
+namespace ANFuck
+{
+	class Transpiler
+	{
+	public:
+		std::string Transpile();
+	};
+}
